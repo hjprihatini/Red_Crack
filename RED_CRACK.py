18aa66e05aa1ccd703f91c2dec27d7ef6e71486c
REDSEC <68374733+hjprihatini@users.noreply.github.com>
2022-03-12 21:08:39 +0700

    Add files via upload

---
+++ RED_CRACK.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import time
+
+os.system("clear")
+os.system("figlet RED_CRACK")
+print
+print("Create : RedSec")
+print("Team : IXP || CySec || MXP")
+print("Youtube : RED SEC")
+print("Github : https://github.com/REDSEC")
+print
+print("[+] Ketik No 1 Untuk mendapatkan akun")
+print
+Pilih = raw_input ('Input == > ')
+print
+if Pilih =="1":
+  os.system("clear")
+print
+os.system("figlet Selesai Cok")
+print
+print("akun ini hasil crack jadi ada yang bisa dan enggak")
+print("untuk update tunggu aja nanti saya pikir")
+print
+print("Link Platinum Only : https://gofile.io/d/qwLfet")
+print("Link Master Only : https://gofile.io/d/1PvDaE")
+print
+print("Semoga Bermanfaat")
+print("By RedSec")
+print
